7e09e155496cb4f887c509b61f3d4a038ccde1b4
Garmash Alexander <sasha.garmash96@gmail.com>
2025-10-02 15:03:17 +0300

    small tweaks

--- public_html/server/app.js
+++ public_html/server/app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public', 'views'));
 
 // pages
 app.get('/', (req, res) => {
